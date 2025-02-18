9fbaef6e8f312307264d20eada71fbd8dfd38220
Bella Solo <bella.solomon-falkfrii@outlook.com>
2025-02-18 10:46:56 +0100

    new connection to azure and modified code

--- BurningLab/BurningLab/Program.cs
+++ BurningLab/BurningLab/Program.cs
@@ -1,5 +1,6 @@
 using BurningLab.Models;
 using BurningLab.Services;
+using Microsoft.AspNetCore.Mvc;
 using MongoDB.Bson;
 
 namespace BurningLab
@@ -15,67 +16,88 @@ namespace BurningLab
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
             var mongoConnectionString = builder.Configuration["ConnectionStrings:MongoDB"];
             var databaseName = "TheBurnLab";
 
-            BurnLabService db = new BurnLabService(mongoConnectionString, databaseName);
 
-            if (app.Environment.IsDevelopment())
-            {
+            var app = builder.Build();
+            //var mongoConnectionString = builder.Configuration["ConnectionStrings:MongoDB"];
+            //var databaseName = "TheBurnLab";
+
+            //BurnLabService db = new BurnLabService(mongoConnectionString, databaseName);
+
+            //if (app.Environment.IsDevelopment())
+            //{
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            //}
 
             app.UseHttpsRedirection();
-            app.UseAuthorization();              
+            app.UseAuthorization();
 
             //CREATE user
-            app.MapPost("/User", async (Users user) =>
+            app.MapPost("/User", async (BurnLabService service, Users user) =>
             {
-                var testDB = await db.CreateUser("Users", user);
-                return Results.Ok(testDB);
-            });
-
-            //READ users
-            app.MapGet("/Users", async () =>
-            {
-                var users = await db.GetUsers("Users");
+                var users = await service.CreateUser("Users", user);
                 return Results.Ok(users);
             });
 
-            //UPDATE user
-            app.MapPut("/User", async (Users updateUser) =>
+
+            //READ users
+            app.MapGet("/Users", async (BurnLabService service) =>
             {
-                var user = await db.UpdateUser("Users", updateUser);
+                var users = await service.GetUsers("Users");
+                return Results.Ok(users);
+            });
+
+
+            //UPDATE user
+            app.MapPut("/User", async (BurnLabService service, Users updateUser) =>
+            {
+                var user = await service.UpdateUser("Users", updateUser);
                 return Results.Ok(user);
             });
 
+
             //DELETE user
-            app.MapDelete("/user/{id}", async (ObjectId id) =>
+            app.MapDelete("/user/{id}", async (BurnLabService service, string id) =>
             {
-                var user = await db.DeleteUser("Users", id);
+                var user = await service.DeleteUser("Users", new ObjectId(id));
                 return Results.Ok(user);
+            });
+
+
+            // Inloggnings endpoint
+            app.MapPost("/signin", async (BurnLabService service, Users loginUser) =>
+            {
+                var users = await service.GetUsers("Users");
+
+                var user = users.FirstOrDefault(u => u.username == loginUser.username);
+
+                if (user == null || user.password != loginUser.password)
+                    return Results.Unauthorized();
+
+                return Results.Ok(new { message = "Inloggad!", userId = user._id });
             });
 
             // Skapa svar
-            app.MapPost("/Answers", async (Answers answer) =>
+            app.MapPost("/Answers", async (BurnLabService service, Answers answer) =>
             {
-                var createdAnswer = await db.CreateAnswer("Answers", answer);
+                var createdAnswer = await service.CreateAnswer("Answers", answer);
                 return Results.Ok(createdAnswer);
             });
 
             // Hämta alla  svar
-            app.MapGet("/Answers", async () =>
+            app.MapGet("/Answers", async (BurnLabService service) =>
             {
-                var answers = await db.GetAnswers("Answers");
+                var answers = await service.GetAnswers("Answers");
                 return Results.Ok(answers);
-            });         
+            });
 
             //hämta alla ideer
-            app.MapGet("/Ideas", async () =>
+            app.MapGet("/Ideas", async (BurnLabService service) =>
             {
-                var idea = await db.GetIdeas("Ideas");
+                var idea = await service.GetIdeas("Ideas");
                 return Results.Ok(idea);
             });
 
--- BurningLab/BurningLab/Services/BurnLabService.cs
+++ BurningLab/BurningLab/Services/BurnLabService.cs
@@ -6,28 +6,28 @@ namespace BurningLab.Services
 {
     public class BurnLabService
     {
-        private IMongoDatabase db;
+        private readonly IMongoDatabase _db;
 
         public BurnLabService(string connectionString, string database)
         {
             var client = new MongoClient(connectionString);
-            db = client.GetDatabase(database);
+            _db = client.GetDatabase(database);
             CreateIndexes().Wait();
         }
 
         private async Task CreateIndexes()
         {
-            var userCollection = db.GetCollection<Users>("Users");
+            var userCollection = _db.GetCollection<Users>("Users");
             var indexKeysDefinition = Builders<Users>.IndexKeys.Ascending(u => u.username);
             var indexModel = new CreateIndexModel<Users>(indexKeysDefinition, new CreateIndexOptions { Unique = true });
             await userCollection.Indexes.CreateOneAsync(indexModel);
         }
 
+
         //CREATE user
         public async Task<List<Users>> CreateUser(string table, Users user)
         {
-            var collection = db.GetCollection<Users>(table);
-
+            var collection = _db.GetCollection<Users>(table);
             try
             {
                 await collection.InsertOneAsync(user);
@@ -43,14 +43,14 @@ namespace BurningLab.Services
         //READ user
         public async Task<List<Users>> GetUsers(string table)
         {
-            var collection = db.GetCollection<Users>(table);
+            var collection = _db.GetCollection<Users>(table);
             return await collection.Find(_ => true).ToListAsync();
         }
 
         //UPDATE user
         public async Task<Users> UpdateUser(string table, Users user)
         {
-            var collection = db.GetCollection<Users>(table);
+            var collection = _db.GetCollection<Users>(table);
             await collection.ReplaceOneAsync(x => x._id == user._id, user);
             return user;
         }
@@ -58,7 +58,7 @@ namespace BurningLab.Services
         //DELETE user
         public async Task<string> DeleteUser(string table, ObjectId id)
         {
-            var collection = db.GetCollection<Users>(table);
+            var collection = _db.GetCollection<Users>(table);
             var result = await collection.DeleteOneAsync(x => x._id == id);
             return result.DeletedCount > 0 ? "Användaren togs bort" : "Användaren hittades inte";
         }
@@ -66,23 +66,25 @@ namespace BurningLab.Services
         //skapa svar
         public async Task<List<Answers>> CreateAnswer(string table, Answers answer)
         {
-            var collection = db.GetCollection<Answers>(table);
+            var collection = _db.GetCollection<Answers>(table);
             await collection.InsertOneAsync(answer);
             return collection.AsQueryable().ToList();
         }
 
+
         // Hämta alla svar
         public async Task<List<Answers>> GetAnswers(string table)
         {
-            var collection = db.GetCollection<Answers>(table);
+            var collection = _db.GetCollection<Answers>(table);
             return await collection.Find(_ => true).ToListAsync();
         }
+
 
         //hämta ideer
         public async Task<List<Ideas>> GetIdeas(string table)
         {
-            var collection = db.GetCollection<Ideas>(table);
+            var collection = _db.GetCollection<Ideas>(table);
             return await collection.Find(_ => true).ToListAsync();
-        }       
+        }
     }
 }
